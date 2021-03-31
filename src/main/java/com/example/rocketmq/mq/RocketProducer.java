7d0641d501ef9eddba8fa08890f3f54c3c55949e
ziyear <superziyear@163.com>
2021-03-31 23:02:55 +0800

    初次提交

--- src/main/java/com/example/rocketmq/mq/RocketProducer.java
+++ src/main/java/com/example/rocketmq/mq/RocketProducer.java
@@ -4,7 +4,7 @@ import com.example.rocketmq.mq.dto.SendObjectDTO;
 import org.apache.rocketmq.client.producer.SendResult;
 
 /**
- * 功能描述 : 匹配
+ * 功能描述 : 生产者
  *
  * @author Ziyear 2021-03-29 21:01
  */
